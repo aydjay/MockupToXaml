b328a538e6f0ea274023d26e03d3cc31b863b81b
aydjay <aydjay@gmail.com>
2018-11-28 15:49:05 +0000

    Handle rudimentary checkbox states

--- MockupToXaml.Converters/CheckBox.cs
+++ MockupToXaml.Converters/CheckBox.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
 using MockupToXaml.Model;
 
 namespace MockupToXaml.Converters
@@ -18,10 +19,28 @@ namespace MockupToXaml.Converters
                 // Add attributes for the mockup control properties
                 var tag = XElement.Parse(code);
                 Utility.ProcessProperty(tag, "Content", "text", control);
-                //Todo Handle state
+
                 if (control.ControlProperties.ContainsKey("state"))
                 {
+                    CheckBoxStateEnum result;
+                    Enum.TryParse(control.ControlProperties["state"], true, out result);
 
+                    switch (result)
+                    {
+                        case CheckBoxStateEnum.Selected:
+                            tag.SetAttributeValue("IsChecked", true);
+                            break;
+                        case CheckBoxStateEnum.Disabled:
+                            tag.SetAttributeValue("IsEnabled", false);
+                            break;
+                        case CheckBoxStateEnum.DisabledSelected:
+                            tag.SetAttributeValue("IsEnabled", false);
+                            tag.SetAttributeValue("IsChecked", true);
+                            break;
+                        default:
+                            tag.SetAttributeValue("Content", "Unsupported Checkbox State");
+                            break;
+                    }
                 }
 
                 return tag.ToString();
@@ -30,6 +49,14 @@ namespace MockupToXaml.Converters
             {
                 return code;
             }
+        }
+
+        private enum CheckBoxStateEnum
+        {
+            Unknown,
+            Selected,
+            Disabled,
+            DisabledSelected,
         }
     }
 }
