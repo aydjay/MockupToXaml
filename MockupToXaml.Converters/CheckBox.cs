b7e14027786ef65bab5029d366f6c3533edcfde1
aydjay <aydjay@gmail.com>
2018-11-28 15:31:02 +0000

    Initial rudimentary checkbox support

---
+++ MockupToXaml.Converters/CheckBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+using MockupToXaml.Model;
+
+namespace MockupToXaml.Converters
+{
+    public class CheckBox : IMockupControlConverter
+    {
+        public MockupTemplate Template { get; set; }
+
+        public string ConvertMockupToXaml(MockupControl control)
+        {
+            var code = string.Empty;
+
+            code = Utility.PerformReplacements(Template.Template, control);
+
+            try
+            {
+                // Add attributes for the mockup control properties
+                var tag = XElement.Parse(code);
+                Utility.ProcessProperty(tag, "CheckBox", "Content", "text", control);
+                //Todo Handle state
+                if (control.ControlProperties.ContainsKey("state"))
+                {
+
+                }
+
+                return tag.ToString();
+            }
+            catch
+            {
+                return code;
+            }
+        }
+    }
+}
--- MockupToXaml.Model/Utility.cs
+++ MockupToXaml.Model/Utility.cs
@@ -4,19 +4,6 @@ namespace MockupToXaml.Model
 {
     public class Utility
     {
-        public static void ProcessProperties(XElement tag, MockupControl control)
-        {
-            if (tag.Name.LocalName == "Button")
-            {
-                ProcessProperty(tag, "Button", "Content", "text", control);
-            }
-
-            if (tag.Name.LocalName == "TextBlock")
-            {
-                ProcessProperty(tag, "TextBlock", "Text", "text", control);
-            }
-        }
-
         public static void ProcessProperty(XElement tag, string xamlControlName, string xamlAttributeName, string mockupPropertyName, MockupControl mockupControl)
         {
             if (!string.IsNullOrEmpty(mockupControl.ControlProperties[mockupPropertyName]))
